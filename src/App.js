435ac6b0893751bb617b35c7395e4299578c8cdf
S Nadeem <venomreborn@gmail.com>
2024-03-01 10:38:44 +0000

    added Handpose model

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+
+// include DOM references
+import React, {useRef} from 'react';
+
 // import tf dependencies
 import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import * as Webcam from "react-webcam";
+
+
 import './App.css';
 
 function App() {
+  // include references to pass
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  // load handpose model
+  const runHandpose = async () => {
+    const net = await handpose.load();
+    console.log('Handpose model loaded.');
+  };
+  runHandpose();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Test Deployed. First Change with dependencies.
-        </p> Second test commmit.
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* webcam */}
+        <Webcam ref={webcamRef} style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 10,
+          width: 700,
+          height: 500
+        }} />
+
+        {/* canvas */}
+        <canvas ref={canvasRef} style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 10,
+          width: 700,
+          height: 500
+        }} />
       </header>
     </div>
   );
